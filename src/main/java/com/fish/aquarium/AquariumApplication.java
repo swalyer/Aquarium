bc9c8d72abc94ea705d4d764776e64fff9102b9e
Swalyer <ramazanishe228@gmail.com>
2024-10-12 22:25:47 +0300

    Initial commit

---
+++ src/main/java/com/fish/aquarium/AquariumApplication.java
@@ -0,0 +1,13 @@
+package com.fish.aquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquariumApplication.class, args);
+	}
+
+}
