67ba3edd95b4f46f1821e92df0b70117f5403335
Swalyer <ramazanishe228@gmail.com>
2024-10-16 18:08:55 +0300

    ready Auth

--- src/main/java/com/fish/aquarium/controller/AquariumController.java
+++ src/main/java/com/fish/aquarium/controller/AquariumController.java
@@ -1,18 +1,17 @@
 package com.fish.aquarium.controller;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowire;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fish.aquarium.entity.Aquarium;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.fish.aquarium.service.AquariumService;
 
 
 
--- src/main/java/com/fish/aquarium/service/AquariumService.java
+++ src/main/java/com/fish/aquarium/service/AquariumService.java
@@ -1,9 +1,12 @@
 package com.fish.aquarium.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.fish.aquarium.entity.Aquarium;
+import com.fish.aquarium.repository.AquariumRepository;
 
 @Service
 public class AquariumService {
